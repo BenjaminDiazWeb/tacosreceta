a8c761dda210d62e2f508d63ef08342abcdae1a5
BenjaminDiazWeb <benjaad733@gmail.com>
2025-12-09 20:35:34 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+function modonocturno() {
+document.body.style.backgroundColor = "black";
+document.getElementById("").style.color= "white";
+}
+
+function infodelcopy() {
+alert("esta pagina fue desarrollada por Lionel Andrés Messi")    
+}
